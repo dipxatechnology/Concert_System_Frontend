ff420ac28aabc25640328fc1840bd7d31558ba92
Abdullah al Sakib <abdullahalsakib903@gmail.com>
2023-11-24 02:25:32 +0800

    Done with FAQ page ✅

--- src/pages/Home/FeaturedCard.jsx
+++ src/pages/Home/FeaturedCard.jsx
@@ -1,8 +1,5 @@
 import { Heading, Text, Stack, Image, Card } from "@chakra-ui/react";
 
-const IMAGE =
-  "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
-
 function FeaturedCard() {
   return (
     <Card
