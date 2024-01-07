82731c365f7d91983b5e99813fbee32d43bd099d
Andra <andralai71@gmail.com>
2024-01-08 01:10:44 +0800

    Format code

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -17,43 +17,43 @@ import "./layout.css";
 export default function Navbar() {
   const navigate = useNavigate();
   return (
-    <div style={{padding: "15px 50px"}} className="background">
-      <Flex alignItems='center'>
-        <Image src="src/assets/logo_dark.svg" boxSize={"60px"}/>
+    <div style={{ padding: "15px 50px" }} className="background">
+      <Flex alignItems="center">
+        <Image src="src/assets/logo_dark.svg" boxSize={"60px"} />
         <Spacer />
         <DesktopNav />
         <Spacer />
         <Stack
-              flex={{ base: 1, md: 0 }}
-              justify={"flex-end"}
-              direction={"row"}
-              spacing={6}
-            >
-              <Button
-                bg={"#222222"}
-                fontSize={"sm"}
-                fontWeight={400}
-                color={"white"}
-                onClick={() => navigate('/signup')}
-              >
-                Sign Up
-              </Button>
-              <Button
-                fontSize={"sm"}
-                fontWeight={600}
-                color={"white"}
-                bg={"#222222"}
-                borderColor={"white"}
-                borderWidth={2}
-                onClick={() => navigate('/login')}
-              >
-                Log In
-              </Button>
-            </Stack>
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          <Button
+            bg={"#222222"}
+            fontSize={"sm"}
+            fontWeight={400}
+            color={"white"}
+            onClick={() => navigate("/signup")}
+          >
+            Sign Up
+          </Button>
+          <Button
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bg={"#222222"}
+            borderColor={"white"}
+            borderWidth={2}
+            onClick={() => navigate("/login")}
+          >
+            Log In
+          </Button>
+        </Stack>
       </Flex>
-      <Divider paddingTop={"15px"}/>
+      <Divider paddingTop={"15px"} />
     </div>
-  )
+  );
 }
 
 const DesktopNav = () => {
@@ -61,11 +61,7 @@ const DesktopNav = () => {
     <Stack direction={"row"} spacing={10} textColor={"white"} fontSize={"lg"}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Link
-            to={navItem.href}
-            fontSize={"sm"}
-            fontWeight={500}
-          >
+          <Link to={navItem.href} fontSize={"sm"} fontWeight={500}>
             {navItem.label}
           </Link>
         </Box>
