510ffb1c6ac5e88325bc63cf217ab5af881a4522
Andra <andralai71@gmail.com>
2024-01-15 12:04:44 +0800

    Added buttons for profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -7,7 +7,11 @@ import {
   Spacer,
   Box,
   Center,
+  Icon,
+  Divider,
 } from "@chakra-ui/react";
+import { RiCalendarEventLine, RiSearch2Line } from "react-icons/ri";
+
 import "./profile.css";
 
 export default function Profile() {
@@ -27,6 +31,34 @@ export default function Profile() {
         <Spacer />
         <Avatar size="2xl" marginRight="5vh" />
       </Flex>
+      <Flex marginTop="5vh">
+        <Button
+          size="lg"
+          bg="brand.100"
+          _hover={{ bg: "brand.200" }}
+          fontWeight="bold"
+          width="15rem"
+          borderRadius="10px"
+          justifyContent="left"
+          fontSize="18px"
+        >
+          <Icon as={RiCalendarEventLine} marginRight="10px" boxSize="20px" />
+          Events:
+        </Button>
+        <Spacer />
+        <Button
+          bg="brand.100"
+          _hover={{ bg: "brand.200" }}
+          fontWeight="bold"
+          borderRadius="10px"
+          width="11rem"
+          marginRight="8px"
+        >
+          <Icon as={RiSearch2Line} marginRight="10px" boxSize="20px" />
+          Search ticket
+        </Button>
+      </Flex>
+      <Divider margin="3vh 0 6vh 0" />
     </div>
   );
 }
