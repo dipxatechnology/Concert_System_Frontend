230d2f7ae4e5fb04f5c2b3e1c2a7118946e597d7
Andra <andralai71@gmail.com>
2024-02-16 12:25:03 +0800

    User login will persist across app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -13,12 +13,20 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import SpecificEvent from "./pages/Events/SpecificEvent";
 import TicketHistory from "./pages/TicketHistory";
-import { useState } from "react";
+
 import "./App.css";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const userData = localStorage.getItem("userData");
+
+    if (userData) {
+      setLoggedIn(true);
+    }
+  }, []);
 
   return (
     <Routes>
