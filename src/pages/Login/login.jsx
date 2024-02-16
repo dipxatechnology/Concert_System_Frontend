3768051a798ce22ddd1e555999418311b32ffa9e
Andra <andralai71@gmail.com>
2024-02-16 11:12:43 +0800

    User can press enter to login or sign up

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,130 +1,131 @@
-import { useState } from 'react';
-import { 
-  Text, 
-  Box, 
-  VStack, 
-  Input, 
-  InputGroup, 
+import { useState } from "react";
+import {
+  Text,
+  Box,
+  VStack,
+  Input,
+  InputGroup,
   InputRightElement,
   Checkbox,
   Button,
   IconButton,
-} from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Link } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import api from '../../api/api';
-import { useNavigate } from 'react-router-dom';
-import './login.css';
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+import api from "../../api/api";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 
 export default function Login({ setLoggedIn }) {
-  const [ show, setShow ] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [show, setShow] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const handleClick = () => setShow(!show);
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
       const loginData = {
         username,
         password,
       };
-  
+
       // Make an API request to authenticate the user and obtain a token
       const response = await api.post("/auth", loginData);
-  
+
       // Extract the token and user data from the response
       const { accessToken, userData } = response.data;
-  
+
       // Store the token and user data in a secure manner (e.g., in cookies or local storage)
       Cookies.set("accessToken", accessToken, { expires: 1 });
       localStorage.setItem("userData", JSON.stringify(userData));
-  
+
       // Set the setLoggedIn to true
       setLoggedIn(true);
-  
+
       // Redirect or perform actions after successful login
       navigate("/");
     } catch (error) {
-      console.error("Login failed:", error.message); 
+      console.error("Login failed:", error.message);
     }
   };
-  
 
-  return(
-    <div 
-      style={{ 
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '70vh',
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
       }}
     >
-      <Box className='login-style' >
-        <VStack alignItems='flex-start'>
-          <Text fontSize='5xl' as='b'>
-            Welcome back!
-          </Text>
-          <Text fontSize='lg' color='grey' marginTop='5px'>
-            Don't have an account?
-            <Link to='/signup'>
-              <Box as='span' color='#D45161' paddingLeft={'5px'}>Sign up</Box>
-            </Link>.
-          </Text>
-            <Input 
-              placeholder='Username'
-              background='#333333' 
-              border='none'
-              marginTop='40px'
-              size='lg'
+      <Box className="login-style">
+        <form onSubmit={handleLogin}>
+          <VStack alignItems="flex-start">
+            <Text fontSize="5xl" as="b">
+              Welcome back!
+            </Text>
+            <Text fontSize="lg" color="grey" marginTop="5px">
+              Don't have an account?
+              <Link to="/signup">
+                <Box as="span" color="#D45161" paddingLeft={"5px"}>
+                  Sign up
+                </Box>
+              </Link>
+              .
+            </Text>
+            <Input
+              placeholder="Username"
+              background="#333333"
+              border="none"
+              marginTop="40px"
+              size="lg"
               onChange={(e) => setUsername(e.target.value)}
             />
             <InputGroup>
-            <Input 
-              placeholder='Password' 
-              background='#333333' 
-              border='none'
-              type={show ? 'text' : 'password'}
-              size='lg'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <InputRightElement>
-            {show ? 
-              <IconButton 
-                icon={<ViewOffIcon color='white'/>} 
-                onClick={handleClick}
-                style={{ backgroundColor: 'transparent' }} 
+              <Input
+                placeholder="Password"
+                background="#333333"
+                border="none"
+                type={show ? "text" : "password"}
+                size="lg"
+                onChange={(e) => setPassword(e.target.value)}
               />
-              : 
-              <IconButton 
-                icon={<ViewIcon color='white'/>} 
-                onClick={handleClick}
-                style={{ backgroundColor: 'transparent' }} 
-              />
-            }
-            </InputRightElement>
-          </InputGroup>
-          <Checkbox 
-            size='lg' 
-            colorScheme='red' 
-            color='grey' 
-            marginTop='20px'
-          >
-            Remember me
-          </Checkbox>
-          <Button  
-            bg='brand.100' 
-            width='100%' 
-            marginTop='20px'
-            _hover={{ bg: 'brand.200' }}
-            fontWeight='bold'
-            onClick={handleLogin}
-          >
-            Log in
-          </Button>
-        </VStack>
+              <InputRightElement>
+                {show ? (
+                  <IconButton
+                    icon={<ViewOffIcon color="white" />}
+                    onClick={handleClick}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                ) : (
+                  <IconButton
+                    icon={<ViewIcon color="white" />}
+                    onClick={handleClick}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                )}
+              </InputRightElement>
+            </InputGroup>
+            <Checkbox size="lg" colorScheme="red" color="grey" marginTop="20px">
+              Remember me
+            </Checkbox>
+            <Button
+              type="submit"
+              bg="brand.100"
+              width="100%"
+              marginTop="20px"
+              _hover={{ bg: "brand.200" }}
+              fontWeight="bold"
+              onClick={handleLogin}
+            >
+              Log in
+            </Button>
+          </VStack>
+        </form>
       </Box>
     </div>
-  ) 
+  );
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -37,7 +37,8 @@ export default function SignUp({ setLoggedIn }) {
     setUsername(`${firstName} ${lastName}`);
   }, [firstName, lastName]);
 
-  const handleSignUp = async () => {
+  const handleSignUp = async (e) => {
+    e.preventDefault();
     if (password === secondPassword) {
       try {
         const signUpData = {
@@ -69,159 +70,162 @@ export default function SignUp({ setLoggedIn }) {
   return (
     <div>
       <Box className="signup-style">
-        <VStack alignItems="flex-start" maxWidthwidth="30%">
-          <Text fontSize="5xl" as="b">
-            Create an account
-          </Text>
-          <Text fontSize="lg" color="grey" marginTop="5px">
-            Already have one?
-            <Link to="/login">
-              <Box as="span" color="#D45161" paddingLeft={"5px"}>
-                Log in
-              </Box>
-            </Link>
-            .
-          </Text>
-          <HStack>
+        <form onSubmit={handleSignUp}>
+          <VStack alignItems="flex-start" maxWidthwidth="30%">
+            <Text fontSize="5xl" as="b">
+              Create an account
+            </Text>
+            <Text fontSize="lg" color="grey" marginTop="5px">
+              Already have one?
+              <Link to="/login">
+                <Box as="span" color="#D45161" paddingLeft={"5px"}>
+                  Log in
+                </Box>
+              </Link>
+              .
+            </Text>
+            <HStack>
+              <Input
+                placeholder="First name"
+                background="#333333"
+                border="none"
+                marginTop="10px"
+                size="lg"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <Input
+                placeholder="Last name"
+                background="#333333"
+                border="none"
+                marginTop="10px"
+                size="lg"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </HStack>
             <Input
-              placeholder="First name"
-              background="#333333"
-              border="none"
-              marginTop="10px"
-              size="lg"
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <Input
-              placeholder="Last name"
-              background="#333333"
-              border="none"
-              marginTop="10px"
-              size="lg"
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </HStack>
-          <Input
-            placeholder="Contact"
-            background="#333333"
-            border="none"
-            marginTop="5px"
-            size="lg"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-          <Input
-            placeholder="Email Address"
-            background="#333333"
-            border="none"
-            marginTop="5px"
-            size="lg"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Textarea
-            placeholder="Residency Address"
-            background="#333333"
-            border="none"
-            marginTop="5px"
-            size="lg"
-            height="15vh"
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <HStack>
-            <Input
-              placeholder="Postcode"
+              placeholder="Contact"
               background="#333333"
               border="none"
               marginTop="5px"
               size="lg"
-              width="40%"
-              onChange={(e) => setPostCode(e.target.value)}
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
             <Input
-              placeholder="Country"
+              placeholder="Email Address"
               background="#333333"
               border="none"
               marginTop="5px"
               size="lg"
-              width="60%"
-              onChange={(e) => setCountry(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          </HStack>
-          <InputGroup>
-            <Input
-              placeholder="Password"
+            <Textarea
+              placeholder="Residency Address"
               background="#333333"
               border="none"
-              type={show ? "text" : "password"}
-              size="lg"
               marginTop="5px"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <InputRightElement marginTop="8px">
-              {show ? (
-                <IconButton
-                  icon={<ViewOffIcon color="white" />}
-                  onClick={handleClick}
-                  style={{ backgroundColor: "transparent" }}
-                />
-              ) : (
-                <IconButton
-                  icon={<ViewIcon color="white" />}
-                  onClick={handleClick}
-                  style={{ backgroundColor: "transparent" }}
-                />
-              )}
-            </InputRightElement>
-          </InputGroup>
-          <InputGroup>
-            <Input
-              placeholder="Confirm password"
-              background="#333333"
-              border="none"
-              type={show ? "text" : "password"}
               size="lg"
-              marginTop="5px"
-              onChange={(e) => setSecondPassword(e.target.value)}
+              height="15vh"
+              onChange={(e) => setAddress(e.target.value)}
             />
-            <InputRightElement marginTop="8px">
-              {showConfirm ? (
-                <IconButton
-                  icon={<ViewOffIcon color="white" />}
-                  onClick={handleClickConfirm}
-                  style={{ backgroundColor: "transparent" }}
-                />
-              ) : (
-                <IconButton
-                  icon={<ViewIcon color="white" />}
-                  onClick={handleClickConfirm}
-                  style={{ backgroundColor: "transparent" }}
-                />
-              )}
-            </InputRightElement>
-          </InputGroup>
-          <Checkbox size="lg" colorScheme="red" color="grey" marginTop="20px">
-            I agree to the
-            <Link to="/terms">
-              <Box as="span" color="#D45161" paddingLeft={"5px"}>
-                Terms of Use
-              </Box>
-            </Link>{" "}
-            &
-            <Link to="/policy">
-              <Box as="span" color="#D45161" paddingLeft={"5px"}>
-                Privacy Policy
-              </Box>
-            </Link>
-          </Checkbox>
-          <Button
-            bg="brand.100"
-            width="100%"
-            marginTop="20px"
-            _hover={{ bg: "brand.200" }}
-            fontWeight="bold"
-            onClick={() => handleSignUp()}
-          >
-            Create account
-          </Button>
-        </VStack>
+            <HStack>
+              <Input
+                placeholder="Postcode"
+                background="#333333"
+                border="none"
+                marginTop="5px"
+                size="lg"
+                width="40%"
+                onChange={(e) => setPostCode(e.target.value)}
+              />
+              <Input
+                placeholder="Country"
+                background="#333333"
+                border="none"
+                marginTop="5px"
+                size="lg"
+                width="60%"
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </HStack>
+            <InputGroup>
+              <Input
+                placeholder="Password"
+                background="#333333"
+                border="none"
+                type={show ? "text" : "password"}
+                size="lg"
+                marginTop="5px"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement marginTop="8px">
+                {show ? (
+                  <IconButton
+                    icon={<ViewOffIcon color="white" />}
+                    onClick={handleClick}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                ) : (
+                  <IconButton
+                    icon={<ViewIcon color="white" />}
+                    onClick={handleClick}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                )}
+              </InputRightElement>
+            </InputGroup>
+            <InputGroup>
+              <Input
+                placeholder="Confirm password"
+                background="#333333"
+                border="none"
+                type={show ? "text" : "password"}
+                size="lg"
+                marginTop="5px"
+                onChange={(e) => setSecondPassword(e.target.value)}
+              />
+              <InputRightElement marginTop="8px">
+                {showConfirm ? (
+                  <IconButton
+                    icon={<ViewOffIcon color="white" />}
+                    onClick={handleClickConfirm}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                ) : (
+                  <IconButton
+                    icon={<ViewIcon color="white" />}
+                    onClick={handleClickConfirm}
+                    style={{ backgroundColor: "transparent" }}
+                  />
+                )}
+              </InputRightElement>
+            </InputGroup>
+            <Checkbox size="lg" colorScheme="red" color="grey" marginTop="20px">
+              I agree to the
+              <Link to="/terms">
+                <Box as="span" color="#D45161" paddingLeft={"5px"}>
+                  Terms of Use
+                </Box>
+              </Link>{" "}
+              &
+              <Link to="/policy">
+                <Box as="span" color="#D45161" paddingLeft={"5px"}>
+                  Privacy Policy
+                </Box>
+              </Link>
+            </Checkbox>
+            <Button
+              type="submit"
+              bg="brand.100"
+              width="100%"
+              marginTop="20px"
+              _hover={{ bg: "brand.200" }}
+              fontWeight="bold"
+              onClick={() => handleSignUp()}
+            >
+              Create account
+            </Button>
+          </VStack>
+        </form>
       </Box>
     </div>
   );
